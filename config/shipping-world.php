cdee85650fcd417173cc3268995056d0fd63637d
Jeremy Cobb <the.jeremy.cobb@gmail.com>
2025-12-12 00:36:05 -0600

    add initial configuration file for default settings and carriers

---
+++ config/shipping-world.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default settings
+    |--------------------------------------------------------------------------
+    */
+
+    'default_country' => 'US',
+
+    // In the future you can toggle carriers, etc.
+    'carriers' => [
+        'ups'   => true,
+        'usps'  => true,
+        'fedex' => true,
+    ],
+];
